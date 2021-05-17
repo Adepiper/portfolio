b365ee26b17d739f35446419223eab6629548f16
adepiper <tobi@roadpreppers.com>
2021-05-17 18:54:13 +0100

    updated portfolio

--- src/views/utilities/Nav.js
+++ src/views/utilities/Nav.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import gsap from 'gsap/gsap-core';
+import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
+// import { gsap } from 'gsap';
 
 const Nav = () => {
+   const [reversed, setReversed] = useState(true);
+   const toggleNav = () => {
+      setReversed(!reversed);
+      const navOpen = document.querySelector('.nav-open');
+      const contact = document.querySelector('.contact');
+      const social = document.querySelector('.social-links');
+      const menu = document.querySelector('nav > p');
+      const logo = document.querySelector('nav a');
+
+      const timeline = gsap.timeline({ paused: true });
+
+      timeline
+         .fromTo(navOpen, 0.5, { y: '-100%' }, { y: 0 })
+         .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 })
+         .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 })
+         .fromTo(menu, 0.5, { color: 'white' }, { color: 'black' })
+         .fromTo(logo, 0.5, { color: 'white' }, { color: 'black' }, '-=0.5');
+
+      reversed ? timeline.play() : timeline.reverse();
+   };
    return (
       <header>
          <nav>
             <NavLink to='/'>
                <h2>Adebayo Oluwatobiloba</h2>
             </NavLink>
-            <p>Menu</p>
+            <p onClick={toggleNav}>Menu</p>
 
             <div className='nav-open'>
                <div className='about-nav'>
